6f9c97a55c9e7a865f96597aa4edbd149648b59a
FxIvan <almendraivan210814@gmail.com>
2025-01-12 12:46:43 -0300

    FIX lint

--- cockroach/entities/cockroachEntity.go
+++ cockroach/entities/cockroachEntity.go
@@ -4,9 +4,8 @@ import "time"
 
 type (
 	InsertCockroachDto struct {
-		Id     uint32 `gorm:"primaryKey;autoIncrement" json:"id"`
-		Amount uint32 `json:"number"
-		`
+		Id        uint32    `gorm:"primaryKey;autoIncrement" json:"id"`
+		Amount    uint32    `json:"number"`
 		CreatedAt time.Time `json:"createdAt"`
 	}
 
